f5f72c89b608d17ddef1ce6131392fc6c2497b24
khousayoub <khous.ayoub@gmail.com>
2016-11-29 23:04:10 +0100

    j'ai oublié de commit -_-'

---
+++ script.js
@@ -0,0 +1,20 @@
+let flip = document.querySelectorAll(".flipper");
+for (let i=0 ; i<flip.length; i++) {
+	flip[i].addEventListener("click", function() {
+		if (window.outerWidth <= "2000") {
+			this.classList.toggle("flipped");
+		}
+		});
+	}
+let cont = document.querySelectorAll(".flip-container");
+	for (let i=0 ; i < cont.length ; i++) {
+		cont[i].addEventListener("click", function () {
+			if (window.outerWidth <= "1024")  {
+				this.classList.remove("flip-container");
+				this.classList.add("flip-container-resp");
+				}
+		});
+}
+
+
+
