5b5efd6966f8741539298f054b8506995e348137
khousayoub <khous.ayoub@gmail.com>
2016-12-04 13:55:54 +0100

    ça fait longtemps que j'ai pas commit

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 let flip = document.querySelectorAll(".flipper");
 for (let i=0 ; i<flip.length; i++) {
 	flip[i].addEventListener("click", function() {
-		if (window.outerWidth <= "2000") {
+		if (window.outerWidth <= "1000") {
 			this.classList.toggle("flipped");
 		}
 		});
 	}
-/*let cont = document.querySelectorAll(".flip-container");
-	for (let i=0 ; i < cont.length ; i++) {
-		cont[i].addEventListener("click", function () {
-			if (window.outerWidth <= "1024")  {
-				this.classList.remove("flip-container");
-				this.classList.add("flip-container-resp");
-				}
-		});
-}*/
+document.querySelector(".collapse").addEventListener("click", function() {
+	document.querySelector(".nav").classList.toggle("nav_vert");
+});
+
 
 
 
